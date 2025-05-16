67aab32cacbd4675070e17cae7ff63f238f00924
David Nodine <davidnodine@gmail.com>
2025-05-16 16:30:46 +0200

    adding styles and delete

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
 
 app = Flask(__name__)
+
+app.secret_key = 'super-secret-key-123'
 
 db_path = os.path.join(os.getcwd(), 'data', 'library.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
@@ -15,13 +17,20 @@ db.init_app(app)
 
 @app.route('/')
 def home():
+    search = request.args.get('search', '').strip()
     sort = request.args.get('sort')
+
+    query = Book.query
+
+    if search:
+        query = query.filter(Book.title.ilike(f"%{search}%"))
+
     if sort == "title":
-        books = Book.query.order_by(Book.title).all()
+        query = query.order_by(Book.title)
     elif sort == "author":
-        books = Book.query.join(Author).order_by(Author.name).all()
-    else:
-        books = Book.query.all()
+        query = query.join(Author).order_by(Author.name)
+
+    books = query.all()
     return render_template("home.html", books=books)
 
 @app.route('/add_author', methods=['GET', 'POST'])
@@ -51,6 +60,24 @@ def add_book():
       db.session.commit()
       message = 'Book added successfully!'
   return render_template("add_book.html", authors=authors, message=message)
+
+@app.route('/book/<int:book_id>/delete', methods=['POST'])
+def delete_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    author = book.author
+
+    db.session.delete(book)
+    db.session.commit()
+
+    if len(author.books) == 0:
+        db.session.delete(author)
+        db.session.commit()
+        flash(f"Book '{book.title}' and author '{author.name}' deleted successfully.")
+    else:
+        flash(f"Book '{book.title}' deleted successfully.")
+
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
